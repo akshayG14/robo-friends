2d07d245f32f69f5376925f98d5c2d5573915fc4
AkshayG <akshayg14@gmail.com>
2020-08-26 23:59:42 +0530

    Code cleanup

--- src/containers/App.js
+++ src/containers/App.js
@@ -21,14 +21,14 @@ class App extends Component {
   };
 
   render() {
-    const filterRobots = this.state.robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    const { robots, searchField } = this.state;
+    const filterRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
 
-    if (this.state.robots.length === 0) {
-      return <h1 className="tc">Loading</h1>
-    } else {
-      return (
+    return (
+      !robots.length ?
+        <h1 className="tc">Loading</h1> :
         <div className="tc">
           <h1 className="f1">Robo Friends</h1>
           <SearchBox searchChange={this.onSearchChange} />
@@ -36,8 +36,7 @@ class App extends Component {
             <CardList robots={filterRobots} />
           </Scroll>
         </div>
-      );
-    }
+    );
   }
 }
 
